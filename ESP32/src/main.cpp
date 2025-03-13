2cb827183c94b69c47b2ed02309f8c98167ad05d
Oshadha Nimantha <159919492+Oshadha-Nimantha@users.noreply.github.com>
2025-03-13 09:25:02 +0530

    add libraries

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <WiFi.h>
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
+#include <PubSubClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SeaLevelPreasure_HPA (1013.25)
+
+Adafruit_BME280 bme;
+
+const char* ssid = "Oshadha's Galaxy Tab S6 Lite";
+const char* password = "bxmu1409";
+const char* mqtt_server = "xxxx";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println(F("BME280 test"));
+
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+  Serial.println("BME280 sensor connected successfully");
+  Serial.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void reconnect() {
+
 }
